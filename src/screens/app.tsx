7c9f54ea470709c538cf9819100b4b0b44174a27
kyochanpy <nasu.9618.0527@gmail.com>
2025-01-19 21:11:24 +0900

    feat: ImageViewer Component

--- src/screens/app.tsx
+++ src/screens/app.tsx
@@ -1,14 +1,18 @@
 import { registerRootComponent } from "expo";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, type ImageSourcePropType } from "react-native";
+
+import { ImageViewer } from "../components/image-viewer";
+
+const PlaceholderImage =
+	require("../assets/images/background-image.png") as ImageSourcePropType;
 
 export const App = () => {
 	return (
 		<View style={styles.container}>
-			<Text style={{ color: "#fff" }}>Hello, World!</Text>
-			<Text style={styles.text}>
-				Open up App.tsx to start working on your app!!
-			</Text>
+			<View style={styles.imageContainer}>
+				<ImageViewer placeholderImageSource={PlaceholderImage} />
+			</View>
 			<StatusBar style="auto" />
 		</View>
 	);
@@ -19,10 +23,10 @@ const styles = StyleSheet.create({
 		flex: 1,
 		backgroundColor: "#25292e",
 		alignItems: "center",
-		justifyContent: "center",
 	},
-	text: {
-		color: "#fff",
+	imageContainer: {
+		flex: 1,
+		paddingTop: 58,
 	},
 });
 
